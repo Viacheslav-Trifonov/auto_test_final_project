e9bff5a827695402650b468bdccb2db95da38371
Viacheslav-Trifonov <trifonovvd.mail@yandex.ru>
2022-06-23 10:10:59 +0400

    stage 4.2.4

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,13 +1,9 @@
 from selenium.webdriver.common.by import By
+from .base_page import BasePage
 
 
-link = "http://selenium1py.pythonanywhere.com/"
+class MainPage(BasePage):
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link.click()
 
-def go_to_login_page(browser):
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
-    
-
-def test_guest_can_go_to_login_page(browser): 
-   browser.get(link) 
-   go_to_login_page(browser) 
